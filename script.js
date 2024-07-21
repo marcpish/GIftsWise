c3285d4e22632d913c310fc1bcf3c775faa59ece
marcpish <164066578+marcpish@users.noreply.github.com>
2024-07-21 17:51:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const typed = new Typed('.multiple-text', {
+        strings: ['mother', 'dad that loves to cookout', 'sibling', 'car loving husband', 'new born baby', 'tech-y boyfriend', 'gym goers', 'movie obsessed wife'],
+        typeSpeed: 65,
+        backSpeed: 55,
+        backDelay: 100,
+        loop: true
+    });
+});
